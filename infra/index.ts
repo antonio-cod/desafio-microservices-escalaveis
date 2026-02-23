04e7db248e0cb0549ba84e945c466d87644f9306
antonio-cod <antonio_carlostads@hotmail.com>
2026-02-23 14:54:59 -0300

    feat: add initial TypeScript configuration and package setup for evento-node-infra
    - Created package.json with dependencies for Pulumi and TypeScript.
    - Added tsconfig.json with strict compiler options and output directory configuration.

---
+++ infra/index.ts
@@ -0,0 +1,55 @@
+// import * as pulumi from "@pulumi/pulumi";
+// import * as aws from "@pulumi/aws";
+// import * as awsx from "@pulumi/awsx";
+
+// // Create an AWS resource (S3 Bucket)
+// const bucket = new aws.s3.Bucket("my-bucket");
+
+// // Export the name of the bucket
+// export const bucketName = bucket.id;
+
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
+import * as docker from "@pulumi/docker-build";
+
+const ordersECRRepository = new awsx.ecr.Repository("orders-ecr", {
+  forceDelete: true,
+});
+
+const ordersERCToken = aws.ecr.getAuthorizationTokenOutput({
+  registryId: ordersECRRepository.repository.registryId,
+});
+
+export const ordersDockerImage = new docker.Image("orders-image", {
+  tags: [
+    pulumi.interpolate`${ordersECRRepository.repository.repositoryUrl}:latest`,
+  ],
+  context: {
+    location: "../app-orders",
+  },
+  push: true,
+  platforms: ["linux/amd64"],
+  registries: [
+    {
+      address: ordersECRRepository.repository.repositoryUrl,
+      username: ordersERCToken.userName,
+      password: ordersERCToken.password,
+    },
+  ],
+});
+
+const cluster = new awsx.classic.ecs.Cluster("app-cluster");
+
+const ordersService = new awsx.classic.ecs.FargateService("fargate-orders", {
+  cluster,
+  desiredCount: 1,
+  waitForSteadyState: false,
+  taskDefinitionArgs: {
+    container: {
+      image: ordersDockerImage.ref,
+      cpu: 256,
+      memory: 512,
+    },
+  },
+});
